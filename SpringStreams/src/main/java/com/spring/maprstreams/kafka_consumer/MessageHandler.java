a2a9cc1047c959204b3870d8c1a8cb726ba59d49
vinaymeghraj <vinaymeghraj@gmail.com>
2017-10-02 15:17:22 -0700

    Updates to Consumer

--- SpringStreams/src/main/java/com/spring/maprstreams/kafka_consumer/MessageHandler.java
+++ SpringStreams/src/main/java/com/spring/maprstreams/kafka_consumer/MessageHandler.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
 
 import java.time.Instant;
 import java.time.ZoneId;
@@ -15,12 +16,20 @@ import java.time.format.FormatStyle;
 @EnableBinding(Sink.class)
 @Configuration
 public class MessageHandler {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MessageHandler.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(MessageHandler.class);
 
-    @StreamListener(Sink.INPUT)
-    public void handle(final ChatMessage message) {
-        final DateTimeFormatter df = DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM).withZone(ZoneId.systemDefault());
-        final String time = df.format(Instant.ofEpochMilli(message.getTime()));
-        LOGGER.info("[{}]: {}", time, message.getContents());
-    }
+
+	/*@StreamListener(Sink.INPUT)
+	public String handle(final ChatMessage message) {
+		final DateTimeFormatter df = DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM).withZone(ZoneId.systemDefault());
+		final String time = df.format(Instant.ofEpochMilli(message.getTime()));
+		LOGGER.info("[{}]: {}", time, message.getContents());
+		System.out.println("Receiving message : " +message.getContents());
+		return message.getContents();
+	}*/
+
+	@ServiceActivator(inputChannel=Sink.INPUT)
+	public void loggerSink(Object payload) {
+		System.out.println("Received : " +payload);
+	}
 }
